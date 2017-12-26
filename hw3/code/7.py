fb0628237a90bcc9dbf56f7f65c747a0aed201d1
YJC <openopentw@gmail.com>
2017-12-27 02:04:01 +0800

    Now complete Q9 on coursera.

---
+++ hw3/code/7.py
@@ -0,0 +1,23 @@
+from math import exp
+
+def E_uv(u, v):
+    return exp(u) + exp(2*v) + exp(u*v) + u**2 - 2*u*v + 2*(v**2) - 3*u - 2*v
+
+def grad_uv(u, v):
+    grad_u = exp(u) + v*exp( u*(v-1) )*exp(u) + 2*u - 2*v - 3
+    grad_v = 2*exp(v)*exp(v) + u*exp( v*(u-1) )*exp(v) - 2*u + 4*v - 2
+    return grad_u, grad_v
+
+def update_uv(u, v, eta):
+    grad_u, grad_v = grad_uv(u, v)
+    u -= eta * grad_u
+    v -= eta * grad_v
+    return u, v
+
+u = 0
+v = 0
+eta = 0.01
+
+for t in range(6):
+    print(t, ':', u, v, E_uv(u, v))
+    u, v = update_uv(u, v, eta)
